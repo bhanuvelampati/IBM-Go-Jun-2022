239a286e3a0233ad3dcd447c10d13ad7e2bf8ef6
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-15 17:28:28 +0530

    modules & packages - part - 1

---
+++ 09-modularity/app.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"modularity-demo/calculator"
+)
+
+func main() {
+	fmt.Println("modularity-demo executed")
+	fmt.Println(calculator.Add(100, 200))
+	fmt.Println(calculator.Subtract(100, 200))
+	fmt.Println("operation count = ", calculator.OpCount())
+}
