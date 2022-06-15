a836d48e33487d6bf577da6f3784cb542a84ae31
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-15 10:58:41 +0530

    function composition

--- 03-functions/05-demo.go
+++ 03-functions/05-demo.go
@@ -8,17 +8,36 @@ import (
 )
 
 func main() {
-	fn := getFn()
+	/* fn := getFn()
 	fn()
 
 	add := getAdd()
-	add(100, 200)
+	add(100, 200) */
 
-	logAdd := logOperation(add)
-	logAdd(100, 200)
+	/*
+		logAdd := logOperation(add)
+		logAdd(100, 200)
 
-	logSubtract := logOperation(subtract)
-	logSubtract(100, 200)
+		logSubtract := logOperation(subtract)
+		logSubtract(100, 200)
+	*/
+
+	//combining log with profiling
+	/*
+		logAdd := logOperation(add)
+		profileWithLogAdd := profileOperation(logAdd)
+		profileWithLogAdd(100, 200)
+
+		logSubtract := logOperation(subtract)
+		profileWithLogSubtract := profileOperation(logSubtract)
+		profileWithLogSubtract(100, 200)
+	*/
+	profileWithLogAdd := profileOperation(logOperation(add))
+	profileWithLogAdd(100, 200)
+
+	profileWithLogSubtract := profileOperation(logOperation(subtract))
+	profileWithLogSubtract(100, 200)
+
 }
 
 func getFn() func() {
@@ -38,6 +57,15 @@ func logOperation(oper func(int, int)) func(int, int) {
 		fmt.Println("before invocation")
 		oper(x, y)
 		fmt.Println("after invocation")
+	}
+}
+
+func profileOperation(oper func(int, int)) func(int, int) {
+	return func(x, y int) {
+		start := time.Now()
+		oper(x, y)
+		elapsed := time.Now().Sub(start)
+		fmt.Println("Elapsed =", elapsed)
 	}
 }
 
