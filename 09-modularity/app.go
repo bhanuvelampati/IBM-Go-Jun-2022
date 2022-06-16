4764cb69ed3c16726099765ef446827997ef3e5d
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-16 10:56:12 +0530

    updates

--- 09-modularity/app.go
+++ 09-modularity/app.go
@@ -6,12 +6,16 @@ import (
 
 	/* mathutils "modularity-demo/calculator/utils" */
 	_ "modularity-demo/calculator/utils"
+
+	"github.com/fatih/color"
 )
 
 func main() {
-	fmt.Println("modularity-demo executed")
+	//fmt.Println("modularity-demo executed")
+	color.Red("modularity-demo executed")
 	fmt.Println(calculator.Add(100, 200))
 	fmt.Println(calculator.Subtract(100, 200))
-	fmt.Println("operation count = ", calculator.OpCount())
+	//fmt.Printf("operation count = %d\n", calculator.OpCount())
+	color.Cyan("operation count = %d\n", calculator.OpCount())
 	//fmt.Println("Is 21 even no ? :", mathutils.IsEven(21))
 }
