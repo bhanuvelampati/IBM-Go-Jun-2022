b8483bce40a820e068cd631347a561665230239b
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-17 12:11:39 +0530

    assignment-10 solution

--- 14-concurrency/assignment-10.go
+++ 14-concurrency/assignment-10.go
@@ -7,3 +7,38 @@
 	Once all the goroutines finished their task the app should print "Done" and exit
 
 */
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+)
+
+func main() {
+	wg := &sync.WaitGroup{}
+
+	if noOfGoroutines, err := strconv.Atoi(os.Args[1]); err != nil {
+		fmt.Println("Invalid arguments. Usage 'assignment {count}")
+		return
+	} else {
+		for i := 1; i <= noOfGoroutines; i++ {
+			wg.Add(1)
+			go fn(i, wg)
+		}
+	}
+	wg.Wait()
+	fmt.Println("Done")
+	fmt.Scanln()
+}
+
+func fn(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("goroutine [%d] started\n", id)
+	time.Sleep(time.Duration(rand.Intn(30)) * time.Second)
+	fmt.Printf("goroutine [%d] completed\n", id)
+}
