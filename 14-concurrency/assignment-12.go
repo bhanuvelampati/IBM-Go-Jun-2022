33defdf2e4311f152496d4840f1610cf9bc869d7
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-17 15:50:44 +0530

    assignment-12 problem statement

---
+++ 14-concurrency/assignment-12.go
@@ -0,0 +1,40 @@
+/* modify this program to generate prime numbers for 20 seconds */
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	end := 50
+	primeNoCh := generatePrimes(3, end)
+	for primeNo := range primeNoCh {
+		fmt.Println(primeNo)
+	}
+	fmt.Println("Done")
+}
+
+func generatePrimes(start, end int) chan int {
+	ch := make(chan int)
+	go func() {
+		for no := start; no <= end; no++ {
+			if isPrime(no) {
+				ch <- no
+				time.Sleep(500 * time.Millisecond)
+			}
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= no/2; i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
+}
